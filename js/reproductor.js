bb025e71cf2e4ae6c501c0b14b0b38723b2d231c
Xavier García <xaviergaro.dev@gmail.com>
2016-03-24 19:13:43 +0100

    Pas 12: Afegits efectes als botons i elements de la llista

--- js/reproductor.js
+++ js/reproductor.js
@@ -1,3 +1,36 @@
-/**
- * Created by xavier on 23/3/16.
- */
+$(document).ready(function () {
+
+    inicialitzarSoundPool('a_sobra', 'assets/audio/a-sobra.mp3')
+
+    $('.sonor').on('mouseenter', function () {
+        reproduirSo('a_sobra');
+    });
+
+
+    inicialitzarSoundPool('selecciona', 'assets/audio/selecciona.mp3')
+
+    $('.sonor').on('click', function () {
+        reproduirSo('selecciona');
+    });
+
+});
+
+var soundPool = {},
+    MAX_SOUNDS = 10;
+
+function inicialitzarSoundPool(nom, url) {
+    soundPool[nom] = {
+        sons: [],
+        actual: 0
+    };
+
+    for (var i = 0; i < MAX_SOUNDS; i++) {
+        soundPool[nom].sons.push(new Audio(url));
+    }
+}
+
+function reproduirSo(nom) {
+    var index = soundPool[nom].actual;
+    soundPool[nom].sons[index % MAX_SOUNDS].play();
+    soundPool[nom].actual++;
+}
